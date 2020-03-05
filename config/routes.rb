73a9c71f6d53b31853ebe8a51082f1da63ad9dd5
venancio <rodrigovenancioverissimo@gmail.com>
2020-03-05 16:49:39 -0400

    Add Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :foods
   resources :meals
   resources :measures
+  resources :users
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
